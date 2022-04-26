2fd02bdaa46b510a690a5bc47187567df5edc208
Spacelnvader <la-douce-64@hotmail.fr>
2022-04-26 09:54:49 +0200

    insertion des images

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,10 +1,15 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+
+    const picture = `assets/photographers/portrait/${portrait}`;
 
     function getUserCardDOM() {
+        // Create new article
         const article = document.createElement( 'article' );
+        article.classList.add("photographer-article");
+
+    
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
