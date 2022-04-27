baf5cd4423ab446f0a410dcc7f7c3ec627e5c797
Spacelnvader <la-douce-64@hotmail.fr>
2022-04-27 10:55:41 +0200

    affichage des infos page principale et css

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,7 +1,7 @@
 function photographerFactory(data) {
     const { name, id, city, country, tagline, price, portrait } = data;
 
-
+    
     const picture = `assets/photographers/portrait/${portrait}`;
 
     function getUserCardDOM() {
@@ -9,14 +9,52 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' );
         article.classList.add("photographer-article");
 
+        const url = `./profile.html?id=${id}`;
+        const link = document.createElement( 'a' );
+        link.setAttribute('href', url);
+
+        const mainContainer = document.createElement('div');
+        mainContainer.setAttribute('tabindex', '0');
+
     
         const img = document.createElement( 'img' );
+        img.classList.add("photographer-article__picture")
         img.setAttribute("src", picture)
+        img.setAttribute('alt', name)
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        h2.classList.add("photographer-article__name");
+        h2.setAttribute('aria-label', name); 
+
+        // Build photographer details section
+        const detailsSection = document.createElement('section');
+        detailsSection.setAttribute('tabindex', '0');
+
+        const location = document.createElement( 'h3' );
+        location.textContent = city + ', ' + country;
+        location.classList.add("photographer-article__location")
+
+
+        const tag = document.createElement( 'span' );
+        tag.textContent = tagline;
+        tag.classList.add("photographer-article__tagline")
+
+        const honorary = document.createElement( 'span' );
+        honorary.textContent = price + "€/jour";
+        honorary.classList.add("photographer-article__price")
+
+        //Append elements to article
+        detailsSection.appendChild(location);
+        detailsSection.appendChild(tag)
+        detailsSection.appendChild(honorary);
+        mainContainer.appendChild(img);
+        mainContainer.appendChild(h2);
+        link.appendChild(mainContainer);
+        link.appendChild(detailsSection)
+        // append article to link
+        article.appendChild(link)
+        return article;
     }
     return { name, picture, getUserCardDOM }
 }
